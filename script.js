02d13e009400709255f69c05be8c38af6a47a373
Zhenni <zhenya.harutyunyan99@gmail.com>
2026-03-24 15:54:17 +0400

    1.Was added fontawesome library, 2.Was added text when loading users data, 3.Was added Go back button, 4.Was added background image and favicon.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const usersBlock = document.getElementById("users-block");
 
 btn.addEventListener("click", () => {
     const usersCount = document.getElementById("users").value;
+    usersBlock.innerHTML = "";
+    const loading = document.createElement("p");
+    loading.textContent = "Loading users...";
+    usersBlock.appendChild(loading);
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => {
             if (!response.ok) {
@@ -14,14 +18,21 @@ btn.addEventListener("click", () => {
             usersBlock.innerHTML = "";
             usersBlock.classList.add("users-displayed");
             usersData.slice(0,usersCount).forEach(user => {
-                const div = document.createElement("div");
-                div.classList.add("user-card");
-                div.innerHTML = `<div>${user.name}</div>
-                                    <div>${user.email}</div>
-                                    <div>${user.phone}</div>
-                                    <div>${user.website}</div>
-                                    <div>${user.company.name}</div>`;
-                usersBlock.appendChild(div);
-            })
+                const userCard = document.createElement("div");
+                userCard.classList.add("user-card");
+                userCard.innerHTML = `<div><i class="fa-solid fa-user"></i><b>${user.name}</b></div>
+                                    <div><i class="fa-solid fa-envelope"></i>${user.email}</div>
+                                    <div><i class="fa-solid fa-phone"></i>${user.phone}</div>
+                                    <div><i class="fa-solid fa-globe"></i>${user.website}</div>
+                                    <div><i class="fa-solid fa-building"></i>${user.company.name}</div>`;
+                usersBlock.appendChild(userCard);
+            });
+            const backBtn = document.createElement("button");
+            backBtn.innerHTML = `<i class="fa-solid fa-chevron-left"></i> Go Back`;
+            backBtn.classList.add("back-btn");
+            backBtn.addEventListener("click", () => {
+                location.reload();
+            });
+            usersBlock.appendChild(backBtn);
         });
 });
