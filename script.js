9011a7bc6f095835b647e534f085c2a26b8fd9c0
Zhenni <zhenya.harutyunyan99@gmail.com>
2026-03-25 10:59:07 +0400

    Final improvements.

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const btn = document.getElementById("btn");
+const USERS_API = 'https://jsonplaceholder.typicode.com/users';
+const loading = document.getElementById("loading");
 const usersBlock = document.getElementById("users-block");
 
-btn.addEventListener("click", () => {
-    const usersCount = document.getElementById("users").value;
-    usersBlock.innerHTML = "";
-    const loading = document.createElement("p");
+function showUsers() {
+    startLoading();
+    generateUsersList();
+}
+
+function startLoading() {
+    document.getElementById('show-users').style.display = "none";
     loading.textContent = "Loading users...";
-    usersBlock.appendChild(loading);
-    fetch("https://jsonplaceholder.typicode.com/users")
+}
+
+function generateUsersList() {
+    fetch(USERS_API)
         .then(response => {
             if (!response.ok) {
+                loading.textContent = "Something went wrong!";
                 throw new Error("Failed to fetch users");
             }
             return response.json();
         })
         .then(usersData => {
-            usersBlock.innerHTML = "";
-            usersBlock.classList.add("users-displayed");
-            usersData.slice(0,usersCount).forEach(user => {
+            usersData.slice(0, document.getElementById("users").value).forEach(user => {
                 const userCard = document.createElement("div");
                 userCard.classList.add("user-card");
                 userCard.innerHTML = `<div><i class="fa-solid fa-user"></i><b>${user.name}</b></div>
@@ -26,13 +31,21 @@ btn.addEventListener("click", () => {
                                     <div><i class="fa-solid fa-globe"></i>${user.website}</div>
                                     <div><i class="fa-solid fa-building"></i>${user.company.name}</div>`;
                 usersBlock.appendChild(userCard);
+                usersBlock.classList.add("users-displayed");
             });
-            const backBtn = document.createElement("button");
-            backBtn.innerHTML = `<i class="fa-solid fa-chevron-left"></i> Go Back`;
-            backBtn.classList.add("back-btn");
-            backBtn.addEventListener("click", () => {
-                location.reload();
-            });
-            usersBlock.appendChild(backBtn);
+            loading.style.display = "none";
+        }).catch(error => {
+            loading.textContent = "Something went wrong!";
+            throw new Error("Failed to fetch users");
         });
-});
+    addBackBtn();
+}
+function addBackBtn() {
+    const backBtn = document.getElementById("back");
+    backBtn.innerHTML = `<i class="fa-solid fa-chevron-left"></i> Go Back`;
+    backBtn.classList.add("back-btn");
+    backBtn.addEventListener("click", () => {
+        location.reload();
+    });
+    document.getElementById("container").appendChild(backBtn);
+}
